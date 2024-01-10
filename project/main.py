a71b45563bcec1ed797423ffa9a4c92dff150194
Alvaro Javier Alvarez Cardenas <ajalvarez75@gmail.com>
2024-01-09 22:52:36 -0500

    improving paths with __file__

--- project/main.py
+++ project/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # uvicorn main:app --reload
 
 # Configure file paths using environment variables
-DATA_DIR = os.getenv('DATA_DIR', 'datasets')
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'datasets')
 FUNCTION_1_2_FILE = os.path.join(DATA_DIR, 'function_1_2.csv')
 FUNCTION_3_4_5_FILE = os.path.join(DATA_DIR, 'function_3_4_5.csv')
 FUNCTION_ITEM_ITEM_FILE = os.path.join(DATA_DIR, 'function_item_item.csv')
